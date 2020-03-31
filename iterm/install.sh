7b4e77aa17cf163eace70063fd9d77af94e4fc77
hmbrg <matthias.hamborg@gmail.com>
2020-03-31 22:38:35 +0200

    adjust iTerm

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# example file get updated during update proccess
 [ "$(uname -s)" != "Darwin" ] && exit 0
-sed "s;/Users/carlos;$HOME;g" \
+sed "s;/Users/matthias;$HOME;g" \
 	"$DOTFILES"/iterm/com.googlecode.iterm2.plist.example >"$DOTFILES"/iterm/com.googlecode.iterm2.plist
 defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES/iterm"
 defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
