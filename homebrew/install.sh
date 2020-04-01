72a7bc3c8f76e1e07af7528abfdeb8c8f59c5bbe
hmbrg <matthias.hamborg@gmail.com>
2020-04-01 15:15:41 +0200

    add software install and remove homebrew

--- homebrew/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if command -v brew >/dev/null 2>&1; then
-	echo "› Homebrew is already installed"
-else
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
--- software/install.sh
+++
@@ -1,5 +0,0 @@
-if command -v brew >/dev/null 2>&1; then
-	echo "› Homebrew is already installed"
-else
-	echo "› Homebrew is not installed"
-fi
